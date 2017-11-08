87403f4f9bc961537397790ab723284b209c3e58
ILoveiammorjj <32142482+ILoveiammorjj@users.noreply.github.com>
2017-11-08 21:44:39 +0300

    Update kr2_3.cpp

--- kr2_3.cpp
+++ kr2_3.cpp
@@ -1,11 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool cmp(pair<int, int> p1, pair<int, int> p2) {
-	if(p1.first > p2.first) return true;
-	if(p1.first == p2.first && p1.second < p2.second) return true;
-	return false;
-}
+
 
 int main() {
 	int n;
@@ -20,7 +16,7 @@ int main() {
 	for(auto it = m.begin(); it != m.end(); ++it) {
 		auto v = it->second;
 		auto cur = v.begin();
-		sort(v.begin(), v.end(), cmp);
+		sort(v.begin(), v.end());
 		for(auto itv = v.begin() + 1; itv != v.end(); ++itv) {
 			if(itv->first + itv->second <= cur->first) {
 				++ans; cur = itv;
