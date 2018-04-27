70ad1306926a0fc744bb119d7a05e467c1df9750
ILoveiammorjj <32142482+ILoveiammorjj@users.noreply.github.com>
2018-04-27 11:41:50 +0300

    Create apply.cpp

---
+++ apply/apply.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+template <class FuncType>
+void Apply( FuncType Func )
+{
+} /* End of 'Apply' function */
+
+template <class FuncType, class FirstType, class ...ArgsType>
+void Apply( FuncType Func, FirstType &First, ArgsType&... Args )
+{
+  Func(First);
+  Apply(Func, Args...);
+} /* End of 'Apply' function */
+
+static void Kek(int &X)
+{
+  X *= 10;
+}
+
+void main( void )
+{
+  int x = 1, y = 2, z = 3, k = 4;
+
+  Apply(Kek, x, y, z, k);
+
+  std::cout << x << ' ' << y << ' ' << z << ' ' << k << std::endl;
+  system("pause");
+}
